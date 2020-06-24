59fe5b4d73bb64ec8e2ac2fafbbf5a8d8a0cf4c3
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-06-24 13:26:17 +0100

    add redux setup

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
+import {Provider} from 'react-redux';
+import {store} from './store/index';
+
 import React from 'react';
 import Navigation from './src/Navigation';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Navigation />
-    </>
+    </Provider>
   );
 };
 
---
+++ store/index.js
@@ -0,0 +1,9 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+import authenticationReducer from './reducers/authentication';
+
+const rootReducer = combineReducers({
+  //   authentication: authenticationReducer,
+});
+
+export const store = createStore(rootReducer, applyMiddleware(reduxThunk));
