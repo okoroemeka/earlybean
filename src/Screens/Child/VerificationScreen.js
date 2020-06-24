441dcf891262b17bf6d76423e662a4bd7b90dea3
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-06-24 11:21:20 +0100

    add child verification screen

---
+++ src/Screens/Child/VerificationScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Platform} from 'react-native';
+import {colors, images} from '../../core';
+import Verification from '../../components/Reusable/Verification';
+
+const VerifationScreen = ({navigation: {navigate}}) => {
+  return (
+    <Verification
+      logoImage={images.childBean}
+      tradeMarkImage={images.childTrade}
+      tradeIconTopPosition="20px"
+      tradeIconLeftPosition="360px"
+      justifyHeaderContent="flex-end"
+      greetingTextColor={colors.black}
+      headerTextColor={colors.childPrimaryColor}
+      welcomeText="Hello"
+      instruction="Enter the token ID sent to your parent mobile number"
+      buttonTextColor={colors.white}
+      buttonColor={colors.childPrimaryColor}
+      handleVerification={() => navigate('ConfirmationScreen')}
+      inputBorderBottomColor={colors.childPrimaryColor}
+    />
+  );
+};
+
+export default VerifationScreen;
